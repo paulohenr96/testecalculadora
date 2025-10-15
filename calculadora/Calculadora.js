fbe5e5e9166906e742be9cfa01d53530bc351169
paulo henriq <paulohsantos281096@gmail.com>
2025-10-14 21:27:06 -0300

    add: calculadora

---
+++ calculadora/Calculadora.js
@@ -0,0 +1,22 @@
+class Calculadora{
+
+
+    adicionar (a,b){
+        return a+b;
+    }
+
+    subtrair (a,b)
+    {
+        return a-b;
+    }
+
+    dividir (a,b){
+        return a/b;
+    }
+    multiplicar (a,b){
+        return a*b;
+    }
+}
+
+module.exports=Calculadora;
+
